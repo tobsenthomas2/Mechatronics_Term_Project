0ff45b8aa26538900c1dd9d7512f246bbfd732e8
sydneygoth <sgothenquist@hotmail.com>
2023-03-02 11:10:32 -0800

    Create XY_Motor_Sweep.py

---
+++ src/XY_Motor_Sweep.py
@@ -0,0 +1,6 @@
+
+#for the z axis, rotate up/down until the camera "X" matches with the position of the motor
+
+#motor positioner
+
+#max sweep of 16 deg, 8 deg from midpoint, break the X's into section; so for 8 x's within 48in range, each is 6" which is 2deg change of motion
